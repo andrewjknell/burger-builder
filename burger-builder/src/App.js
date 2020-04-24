5920961fd862adb338c61663441b8d82c08baaac
andrewjknell <andrewjknell@yahoo.com>
2020-04-24 09:17:45 -0700

    new aux

--- burger-builder/src/App.js
+++ burger-builder/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import classes from './App.css';
-import Layout from '../components/Layout/Layout';
-import BurgerBuilder from './BurgerBuilder/BurgerBuilder';
+import Layout from './components/Layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
 class App extends Component {
 
