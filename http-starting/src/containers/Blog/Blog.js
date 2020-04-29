0cc96daeed6f8378765a3986963d6316bb7fe169
andrewjknell <andrewjknell@yahoo.com>
2020-04-29 15:56:41 -0700

    blog app ajax

--- http-starting/src/containers/Blog/Blog.js
+++ http-starting/src/containers/Blog/Blog.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
@@ -6,16 +7,56 @@ import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
-    render () {
+    state = {
+        posts: [],
+        selectPostId: null,
+        error: false,
+    }
+
+    componentDidMount() {
+        axios.get('/posts')
+            .then(response => {
+                const posts = response.data.slice(0, 4);
+                const updatedPosts = posts.map(post => {
+                    return {
+                        ...post,
+                        author: 'Max',
+                    }
+                })
+                this.setState({ posts: updatedPosts })
+            })
+            .catch(err => {
+                this.setState({ error: true })
+            })
+
+    }
+
+    postClickHandler = (id) => {
+        this.setState({ selectPostId: id });
+    }
+
+    render() {
+        let posts = <p style={{ textAlign: 'center' }}>Something went wrong!</p>
+        if (!this.state.error) {
+            posts = this.state.posts.map(post => {
+                return <Post
+                    key={post.id}
+                    title={post.title}
+                    author={post.author}
+                    clicked={() => this.postClickHandler(post.id)}
+                />
+            })
+        }
+
         return (
             <div>
                 <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                    {posts}
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost
+                        id={this.state.selectPostId}
+                    />
                 </section>
                 <section>
                     <NewPost />
