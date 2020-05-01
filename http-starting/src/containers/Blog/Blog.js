1d518ae62082796108359ce80018aad914787c21
andrewjknell <andrewjknell@yahoo.com>
2020-05-01 16:06:51 -0700

    routing added

--- http-starting/src/containers/Blog/Blog.js
+++ http-starting/src/containers/Blog/Blog.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react';
 import axios from 'axios';
 
 import Post from '../../components/Post/Post';
-import FullPost from '../../components/FullPost/FullPost';
-import NewPost from '../../components/NewPost/NewPost';
+import FullPost from '../FullPost/FullPost';
+import NewPost from '../NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
@@ -49,17 +49,17 @@ class Blog extends Component {
         }
 
         return (
-            <div>
+            <div className='Blog'>
+                <header>
+                    <nav>
+                        <ul>
+                            <li><a href='/'>Home</a></li>
+                            <li><a href='/newpost'>New Post</a></li>
+                        </ul>
+                    </nav>
+                </header>
                 <section className="Posts">
                     {posts}
-                </section>
-                <section>
-                    <FullPost
-                        id={this.state.selectPostId}
-                    />
-                </section>
-                <section>
-                    <NewPost />
                 </section>
             </div>
         );
