939caeef97a33df8eb2881a4d0a4e485dbedbc93
andrewjknell <andrewjknell@yahoo.com>
2020-05-13 11:19:40 -0700

    reducer working

---
+++ redux/redux.basic.js
@@ -0,0 +1,17 @@
+const redux = require('redux');
+const createStore = redux.createStore;
+
+// reducer
+const rootReducer = (state, action) => {
+    return state;
+}
+
+// store
+const store = createStore(rootReducer);
+console.log(store.getState())
+
+// dispatching action 
+
+
+// subscription
+
