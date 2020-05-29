beb37eb01b82dfd4982a31969726aa17471d6811
andrewjknell <andrewjknell@yahoo.com>
2020-05-29 09:15:40 -0700

    logout added

--- burger-builder/src/hoc/Layout/Layout.js
+++ burger-builder/src/hoc/Layout/Layout.js
@@ -3,9 +3,9 @@ import Auxilary from '../Auxilary/Auxilary';
 import classes from './Layout.css';
 import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
 import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer';
+import { connect } from 'react-redux';
 
-
-class layout extends Component {
+class Layout extends Component {
     state = {
         showSideDrawer: false,
     }
@@ -23,8 +23,15 @@ class layout extends Component {
     render() {
         return (
             <Auxilary>
-                <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler} />
-                <SideDrawer open={this.state.showSideDrawer} closed={this.sideDrawerCloseHandler} />
+                <Toolbar
+                    isAuth={this.props.isAuthenticated}
+                    drawerToggleClicked={this.sideDrawerToggleHandler}
+                />
+                <SideDrawer
+                    open={this.state.showSideDrawer}
+                    closed={this.sideDrawerCloseHandler}
+                    isAuth={this.props.isAuthenticated}
+                />
                 <main className={classes.Content}>
                     {this.props.children}
                 </main>
@@ -33,4 +40,10 @@ class layout extends Component {
     }
 };
 
-export default layout;
+const mapStateToProps = state => {
+    return {
+        isAuthenticated: state.auth.token === null,
+    }
+}
+
+export default connect(mapStateToProps)(Layout);
